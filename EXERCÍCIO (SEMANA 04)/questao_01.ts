03fbf23329671e7598dd1132ecf892ca8f77353b
Mayck-Rocha <85122860+Mayck-Rocha@users.noreply.github.com>
2021-10-12 20:14:43 -0300

    Add files via upload

---
+++ EXERCÍCIO (SEMANA 04)/questao_01.ts
@@ -0,0 +1,3 @@
+let num_par = n => (n % 2 == 0)
+console.log(num_par(2)) // true
+console.log(num_par(3)) // false
---
+++ EXERCÍCIO (SEMANA 04)/questao_03.ts
@@ -0,0 +1,2 @@
+const array  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+console.log(array.map(function num_par(n) { return n % 2 == 0 }))
